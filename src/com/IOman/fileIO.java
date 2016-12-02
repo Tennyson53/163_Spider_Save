1dc3658ffbacdacc00dd7c899f98224e74da7f3a
Tennyson <tenny@Tenny-MacBook-Pro.local>
2016-12-02 14:36:15 +0800

    161202 updated: finish v1 coding

--- src/com/IOman/fileIO.java
+++
@@ -1,8 +0,0 @@
-package com.IOman;
-
-/**
- * Created by tenny on 2016/11/29.
- */
-public class fileIO {
-
-}
--- src/com/app/mainFunc.java
+++ src/com/app/mainFunc.java
@@ -2,6 +2,9 @@ package com.app;
 
 import com.structure.urlTuple;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 /**
@@ -10,22 +13,47 @@ import java.util.ArrayList;
 public class mainFunc {
 
     public static void main(String[] args) {
+
         int _begin = 0;
         int _end = 10;
 
         ArrayList<urlTuple> urlTups = importIDs.getTuples(_begin, _end);
+
         urlTuple tup;
-        String target_url;
-        String target_text;
-        for (int i = 0; i < _end - _begin; i++) {
-            tup = urlTups.get(i);
 
-            //Generate the target url
-            target_url = getText.getHTML(tup);
+        String url = "jdbc:mysql://localhost:3306/tenny_announcement";
+        String name = "java.sql.Driver";
+        String user = "root";
+        String password = "passwd";
 
-            target_text = getText.getContent(target_url);
-            System.out.println(target_text);
+        String store_text = "insert into Announcement_html(Stock_ID,Announcement_ID,Publish_Date,html) values(?,?,?,?)";
 
+        String ann_url, ann_text;
+        try {
+            Class.forName(name);
+            Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement sqlQuery = conn.prepareStatement(store_text);
+
+            for (int i = 0; i < _end - _begin; i++) {
+                tup = urlTups.get(i);
+                ann_url = getText.getHTML(tup);
+                ann_text = getText.getContent(ann_url);
+                sqlQuery.setString(1, tup.Stock_ID);
+                sqlQuery.setString(2, tup.Announcement_ID);
+                sqlQuery.setString(3, tup.Publish_Date);
+                sqlQuery.setString(4, ann_text);
+
+                int resCode = sqlQuery.executeUpdate();
+                if (resCode > 0) {
+                    System.out.println("Complete Record " + i);
+                } else {
+                    System.out.println("ERROR");
+                    return;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
 
